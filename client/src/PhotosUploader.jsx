3763afa2a436bb58ff5efd9458d3de4eb03c1804
imtiyaj786 <imtiyaj1523@gmail.com>
2023-07-03 12:42:09 +0530

    remove and start icon added in images

--- client/src/PhotosUploader.jsx
+++ client/src/PhotosUploader.jsx
@@ -32,6 +32,12 @@ export default function PhotosUploader({ addedPhotos, onChange }) {
         });
       });
   }
+
+  // for remove photo from edit section
+  function removePhoto(ev, filename) {
+    ev.preventDefault();
+  }
+
   return (
     <>
       <div className="flex gap-2">
@@ -52,12 +58,47 @@ export default function PhotosUploader({ addedPhotos, onChange }) {
         {/* here uploaded images shows */}
         {addedPhotos.length > 0 &&
           addedPhotos.map((link) => (
-            <div key={link} className="h-32 flex">
+            <div key={link} className="h-32 flex relative">
               <img
                 className="rounded-2xl w-full object-cover"
                 src={`http://localhost:4000/uploads/${link}`}
                 alt="Added photo"
               />
+              {/* for remove button */}
+              <button
+                onClick={(ev) => removePhoto(ev, link)}
+                className="cursor-pointer absolute bottom-1 right-1 text-white bg-black bg-opacity-50 rounded-2xl p-2 px-3"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="w-6 h-6"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
+                  />
+                </svg>
+              </button>
+              {/* for select main photo */}
+              <button className="cursor-pointer absolute bottom-1 left-1 text-white bg-black bg-opacity-50 rounded-2xl py-2 px-3">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  fill="currentColor"
+                  className="w-6 h-6"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.007 5.404.433c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.433 2.082-5.006z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+              </button>
             </div>
           ))}
         <label className="h-32 cursor-pointer flex items-center gap-1 justify-center border bg-transparent rounded-2xl p-2 text-2xl text-gray-600">
