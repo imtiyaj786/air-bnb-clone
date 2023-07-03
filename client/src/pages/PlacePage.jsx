b998b02497fdab724d414228aa77a5f523c362ef
imtiyaj786 <imtiyaj1523@gmail.com>
2023-07-03 17:56:14 +0530

    images gallery UI done

---
+++ client/src/pages/PlacePage.jsx
@@ -0,0 +1,91 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { useParams } from "react-router-dom";
+
+export default function PlacePage() {
+  const { id } = useParams();
+  const [place, setPlace] = useState(null);
+  const [showAllPhotos, setShowAllPhotos] = useState(false);
+  useEffect(() => {
+    if (!id) {
+      return;
+    }
+    axios.get(`/places/${id}`).then((response) => {
+      setPlace(response.data);
+    });
+  }, [id]);
+
+  if (!place) return "";
+
+  if (showAllPhotos) {
+    return <div className="">Clicked</div>;
+  }
+
+  return (
+    <div className="mt-4 bg-gray-100 -mx-8 px-8 py-8">
+      <h1 className="text-3xl">{place.title}</h1>
+      <a
+        className="block font-semibold underline"
+        href={"https://maps.google.com/?q=" + place.address}
+        target="_blank"
+      >
+        {place.address}
+      </a>
+      {/* for showing images */}
+      <div className="relative">
+        <div className="grid gap-2 grid-cols-[2fr_1fr] rounded-3xl overflow-hidden">
+          <div>
+            {place.photos?.[0] && (
+              <div>
+                <img
+                  onClick={() => setShowAllPhotos(true)}
+                  className="aspect-square cursor-pointer object-cover"
+                  src={"http://localhost:4000/uploads/" + place.photos[0]}
+                  alt=""
+                />
+              </div>
+            )}
+          </div>
+          <div className="grid">
+            {place.photos?.[1] && (
+              <img
+                onClick={() => setShowAllPhotos(true)}
+                className="aspect-square cursor-pointer object-cover"
+                src={"http://localhost:4000/uploads/" + place.photos[1]}
+                alt=""
+              />
+            )}
+            <div className="overflow-hidden">
+              {place.photos?.[2] && (
+                <img
+                  onClick={() => setShowAllPhotos(true)}
+                  className="aspect-square cursor-pointer object-cover relative top-2"
+                  src={"http://localhost:4000/uploads/" + place.photos[2]}
+                  alt=""
+                />
+              )}
+            </div>
+          </div>
+        </div>
+        <button
+          onClick={() => setShowAllPhotos(true)}
+          className="flex gap-1 absolute bottom-2 right-2 py-2 px-4 bg-white rounded-2xl shadow shadow-md shadow-gray-500"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="currentColor"
+            className="w-6 h-6"
+          >
+            <path
+              fillRule="evenodd"
+              d="M1.5 6a2.25 2.25 0 012.25-2.25h16.5A2.25 2.25 0 0122.5 6v12a2.25 2.25 0 01-2.25 2.25H3.75A2.25 2.25 0 011.5 18V6zM3 16.06V18c0 .414.336.75.75.75h16.5A.75.75 0 0021 18v-1.94l-2.69-2.689a1.5 1.5 0 00-2.12 0l-.88.879.97.97a.75.75 0 11-1.06 1.06l-5.16-5.159a1.5 1.5 0 00-2.12 0L3 16.061zm10.125-7.81a1.125 1.125 0 112.25 0 1.125 1.125 0 01-2.25 0z"
+              clipRule="evenodd"
+            />
+          </svg>
+          Show more photos
+        </button>
+      </div>
+    </div>
+  );
+}
