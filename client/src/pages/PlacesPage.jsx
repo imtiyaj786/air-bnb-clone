ebde887299cb39817c2968d0f9dc83fad67d0fcf
imtiyaj786 <imtiyaj1523@gmail.com>
2023-07-04 15:50:59 +0530

    placesPage UI bug fixes

--- client/src/pages/PlacesPage.jsx
+++ client/src/pages/PlacesPage.jsx
@@ -47,11 +47,11 @@ export default function PlacesPage() {
               className="flex mt-2 cursor-pointer gap-4 bg-gray-100 p-4 rounded-2xl"
               key={place}
             >
-              <div className="flex m-32 h-32 bg-gray-300 grow shrink-0">
+              <div className="flex w-48 bg-gray-300 grow shrink-0">
                 <PlaceImg place={place} />
               </div>
               <div className="grow-0 shrink">
-                <h2 className="text-xl">{place.title}</h2>
+                <h2 className="text-xl font-semibold">{place.title}</h2>
                 <p className="text-sm mt-2">{place.description}</p>
               </div>
             </Link>
